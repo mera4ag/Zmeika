20e3d32c5e73bbcf26ac5cd92ba3f6a246ba9fe6
K0T <104854965+mera4ag@users.noreply.github.com>
2023-02-22 00:08:24 +0200

    Update main.py
    немного изменил

--- Zmeika/main.py
+++ Zmeika/main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
-
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
 
 class Menu(Screen):
     ...
@@ -8,7 +9,20 @@ class Menu(Screen):
 class Game(Screen):
     ...
 
-class VictApp(App):
+class Pos(Widget):
+    # def __init__(self, x,y, size):
+    #     super().__init__()
+    #     self.size = (size, size)
+    #     self.pos = (x,y)
+    ...
+
+# class Game1(Widget):
+#     def __init__(self):
+#         super().__init__(self)
+#         self.poss = Pos(120,120, 40)
+#         self.add_widget(self.poss)
+
+class ZmeikaApp(App):
     def build(self):
         self.manager = ScreenManager()
 
@@ -17,6 +31,5 @@ class VictApp(App):
 
         return self.manager
 
-app = VictApp()
-
-app.run()
+if __name__ == '__main__':
+    ZmeikaApp().run()
